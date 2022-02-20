5b2411dd22ecd62f8f564d29bc5f18e5db1de7e5
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-20 10:49:07 -0500

    Added the macros for exporting symbols.

---
+++ include/scp/macros.hpp
@@ -0,0 +1,19 @@
+#ifndef A284414F_C237_4968_9390_D8DD2BB1203A
+#define A284414F_C237_4968_9390_D8DD2BB1203A
+
+// This file contains all of the macros that will be used by almost everything 
+// in the framework. Currently, it only contains the macro used for exporting 
+// symbols when building the framework as a DLL.
+
+// Macro for exporting the symbols when building as a DLL.
+#ifdef SCP_DLL
+    #ifdef SCP_BUILD_DLL
+        #define SCPGF __declspec(dllexport)
+    #else
+        #define SCPGF __declspec(dllimport)
+    #endif // SCP_BUILD_DLL
+#else
+    #define SCPGF
+#endif // SCP_DLL
+
+#endif /* A284414F_C237_4968_9390_D8DD2BB1203A */
