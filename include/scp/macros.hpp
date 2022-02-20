c3a680bf1245e7acbf197f44315df29838c8dcdb
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-02-20 10:49:24 -0500

    Added pragma once.

--- include/scp/macros.hpp
+++ include/scp/macros.hpp
@@ -1,6 +1,8 @@
 #ifndef A284414F_C237_4968_9390_D8DD2BB1203A
 #define A284414F_C237_4968_9390_D8DD2BB1203A
 
+#pragma once
+
 // This file contains all of the macros that will be used by almost everything 
 // in the framework. Currently, it only contains the macro used for exporting 
 // symbols when building the framework as a DLL.
