2cff3fb234a6e9e839b18268b69ebd0197c4af10
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-02-20 23:47:19 -0500

    Created a basic window class.

---
+++ include/scp/window.hpp
@@ -0,0 +1,60 @@
+#ifndef C23D629F_D9B0_4EDE_8118_1831B5064AE5
+#define C23D629F_D9B0_4EDE_8118_1831B5064AE5
+
+#include <string_view>
+#include <cstdint>
+
+#include <scp/macros.hpp>
+
+// Forward declarations
+struct GLFWwindow;
+
+namespace scp
+{
+    // A simple class for displaying a window onto the screen. Currently uses 
+    // GLFW as it's backend. As with all other classes, I will add features li-
+    // ttle by little as I need them.
+    class window_t
+    {
+    public:
+        // Initialize GLFW
+        static void init_glfw();
+        
+        // The width and height are specified in pixels. The title is self-exp-
+        // lanitory.
+        window_t(uint16_t width, uint16_t height, std::string_view title);
+        
+        // Show the window, since it's hidden by default.
+        void show() const;
+        
+        // Returns if the window is still open.
+        bool is_open() const;
+        
+        // Returns the width of the window.
+        uint16_t get_width() const;
+        
+        // Returns the height of the window.
+        uint16_t get_height() const;
+        
+        // Note: destructor only destroys the window, but does not terminates 
+        // GLFW.
+        ~window_t();
+        
+        // Terminate GLFW
+        static void terminate_glfw();
+        
+    private:
+        // Callbacks:
+        // None, yet, but they will be added once the event system is in place.
+        
+        // The actual window handle.
+        GLFWwindow* m_window;
+        
+        // The window width and height, since retrieving them via GLFW is kind 
+        // of wierd
+        uint16_t m_width;
+        uint16_t m_height;
+    };
+}
+
+#endif /* C23D629F_D9B0_4EDE_8118_1831B5064AE5 */
---
+++ src/window.cpp
@@ -0,0 +1,54 @@
+#include <GLFW/glfw3.h>
+#include <stdexcept>
+
+#include <scp/window.hpp>
+
+using scp::window_t;
+
+void window_t::init_glfw()
+{
+    if (!glfwInit())
+    {
+        throw std::runtime_error("Failed to initialize GLFW.");
+    }
+}
+
+window_t::window_t(uint16_t p_width, uint16_t p_height, std::string_view p_title)
+{
+    glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+    
+    m_width = p_width;
+    m_height = p_height;
+    
+    m_window = glfwCreateWindow(p_width, p_height, p_title.data(), nullptr, nullptr);
+    if (m_window == nullptr)
+    {
+        terminate_glfw();
+        throw std::runtime_error("Failed to create GLFW window " + std::string(p_title.data()));
+    }
+}
+
+void window_t::show() const
+{
+    glfwShowWindow(m_window);
+}
+
+bool window_t::is_open() const
+{
+    return !glfwWindowShouldClose(m_window);
+}
+
+uint16_t window_t::get_width() const
+{
+    return m_width;
+}
+
+uint16_t window_t::get_height() const
+{
+    return m_height;
+}
+
+window_t::~window_t()
+{
+    glfwDestroyWindow(m_window);
+}
