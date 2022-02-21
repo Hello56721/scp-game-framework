1d7f6bdd4703ab4d2c0d87b420f5860af8f27f44
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-02-20 23:53:37 -0500

    Tested the window class (it works.)

---
+++ tests/window.cpp
@@ -0,0 +1,15 @@
+#include <scp/window.hpp>
+
+int main()
+{
+    scp::window_t::init_glfw();
+    
+    scp::window_t window(1280, 720, "SCP Game Framework Window Test");
+    
+    window.show();
+    
+    while (window.is_open())
+    {
+        scp::window_t::poll_events();
+    }
+}
